d63a00a7e73d4715905d8a5d5ad1c84b8396e682
Tankten <3314921154@qq.com>
2023-06-29 19:29:34 +0800

    张振

---
+++ src/main/java/com/aistar/entity/Student.java
@@ -0,0 +1,17 @@
+package com.aistar.entity;
+
+public class Student {
+    private int id;
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
